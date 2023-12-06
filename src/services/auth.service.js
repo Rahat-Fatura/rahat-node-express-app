adf89c7b7c582cad7e9f8002483844b2ca03f31d
Berkay Gökçe <berkaygkc7@gmail.com>
2023-12-06 12:40:20 +0300

    first commit of boilerplate

---
+++ src/services/auth.service.js
@@ -0,0 +1,104 @@
+const httpStatus = require('http-status');
+const tokenService = require('./token.service');
+const userService = require('./user.service');
+const logger = require('../config/logger');
+const Token = require('../utils/database').token;
+const ApiError = require('../utils/ApiError');
+const { tokenTypes } = require('../config/tokens');
+
+/**
+ * Login with username and password
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<User>}
+ */
+const loginUserWithEmailAndPassword = async (email, password) => {
+  const user = await userService.getUserByEmail(email);
+  if (!user || !(await userService.isPasswordMatch(password, user))) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Kullanıcı adı veya şifre hatalı');
+  }
+  return user;
+};
+
+/**
+ * Logout
+ * @param {string} refreshToken
+ * @returns {Promise}
+ */
+const logout = async (refreshToken) => {
+  const refreshTokenDoc = await Token.findFirst({
+    where: { token: refreshToken, type: tokenTypes.REFRESH, blacklisted: false },
+  });
+  if (!refreshTokenDoc) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Böyle bir token bulunamadı');
+  }
+  await Token.delete({ where: { id: refreshTokenDoc.id } });
+};
+
+/**
+ * Refresh auth tokens
+ * @param {string} refreshToken
+ * @returns {Promise<Object>}
+ */
+const refreshAuth = async (refreshToken) => {
+  try {
+    const refreshTokenDoc = await tokenService.verifyToken(refreshToken, tokenTypes.REFRESH);
+    const user = await userService.getUserById(refreshTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await Token.delete({ where: { id: refreshTokenDoc.id } });
+    return tokenService.generateAuthTokens(user);
+  } catch (error) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Lütfen tekrar giriş yapın');
+  }
+};
+
+/**
+ * Reset password
+ * @param {string} resetPasswordToken
+ * @param {string} newPassword
+ * @returns {Promise}
+ */
+const resetPassword = async (resetPasswordToken, newPassword) => {
+  try {
+    const resetPasswordTokenDoc = await tokenService.verifyToken(resetPasswordToken, tokenTypes.RESET_PASSWORD);
+    const user = await userService.getUserById(resetPasswordTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await userService.updateUserById(user.id, { password: newPassword });
+    await Token.deleteMany({ where: { user_id: user.id, type: tokenTypes.RESET_PASSWORD } });
+  } catch (error) {
+    logger.error(error);
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Şifre sıfırlama başarısız oldu');
+  }
+};
+
+/**
+ * Verify email
+ * @param {string} verifyEmailToken
+ * @returns {Promise}
+ */
+const verifyEmail = async (verifyEmailToken) => {
+  try {
+    const verifyEmailTokenDoc = await tokenService.verifyToken(verifyEmailToken, tokenTypes.VERIFY_EMAIL);
+    const user = await userService.getUserById(verifyEmailTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await Token.deleteMany({ where: { user_id: user.id, type: tokenTypes.VERIFY_EMAIL } });
+    await userService.updateUserById(user.id, { isEmailVerified: true });
+  } catch (error) {
+    logger.error(error);
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'e-Posta doğrulama başarısız oldu');
+  }
+};
+
+module.exports = {
+  loginUserWithEmailAndPassword,
+  logout,
+  refreshAuth,
+  resetPassword,
+  verifyEmail,
+};
---
+++ src/services/user.service.js
@@ -0,0 +1,107 @@
+const httpStatus = require('http-status');
+const bcrypt = require('bcryptjs');
+const ApiError = require('../utils/ApiError');
+const User = require('../utils/database').user;
+
+/**
+ * Create a user
+ * @param {Object} userBody
+ * @returns {Promise<User>}
+ */
+const createUser = async (userBody) => {
+  if (await User.findFirst({ where: { email: userBody.email } })) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'e-Posta daha önce alınmış');
+  }
+  const userdata = Object.assign(userBody, { password: await bcrypt.hash(userBody.password, 8) });
+  const user = await User.create({ data: userdata });
+  return user;
+};
+
+/**
+ * Get user by id
+ * @param {ObjectId} id
+ * @returns {Promise<User>}
+ */
+const getUserById = async (id) => {
+  const user = await User.findFirst({ where: { id } });
+  return user;
+};
+
+/**
+ * Get user by email
+ * @param {string} email
+ * @returns {Promise<User>}
+ */
+const getUserByEmail = async (email) => {
+  const user = await User.findFirst({ where: { email } });
+  return user;
+};
+
+/**
+ * Update user by id
+ * @param {ObjectId} userId
+ * @param {Object} updateBody
+ * @returns {Promise<User>}
+ */
+const updateUserById = async (userId, updateBody) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Kullanıcı bulunamadı');
+  }
+  const newBody = updateBody;
+  if (newBody.email && (await User.findFirst({ where: { email: newBody.email, NOT: { id: userId } } }))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'e-Posta daha önce alınmış');
+  }
+  if (newBody.password) {
+    newBody.password = await bcrypt.hash(newBody.password, 8);
+  }
+  Object.assign(user, newBody);
+  await User.update({ where: { id: userId }, data: user });
+  return user;
+};
+
+/**
+ * Delete user by id
+ * @param {ObjectId} userId
+ * @returns {Promise<User>}
+ */
+const deleteUserById = async (deleteUserId, fromByUserId) => {
+  if (deleteUserId === fromByUserId) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Kendi hesabınızı silemezsiniz');
+  }
+  const user = await getUserById(deleteUserId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  await User.delete({ where: { id: deleteUserId } });
+  return user;
+};
+
+/**
+ * Get users list
+ * @returns {Promise<User>}
+ */
+const getUsersList = async () => {
+  const users = await User.findMany();
+  return users;
+};
+
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @param {Object} user
+ * @returns {Promise<boolean>}
+ */
+const isPasswordMatch = async (password, user) => {
+  return bcrypt.compare(password, user.password);
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+  getUserByEmail,
+  updateUserById,
+  deleteUserById,
+  getUsersList,
+  isPasswordMatch,
+};
